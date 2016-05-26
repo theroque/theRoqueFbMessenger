758a1a3c1d292d88f87966e4497e735559365604
Nikita Beskodarov <buptyozzz@gmail.com>
2016-05-26 17:04:11 +0300

    Signed-off-by: Nikita Beskodarov <buptyozzz@gmail.com>

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ controller.setupWebserver(port, function (trouble, webserver) {
 	});
 });
 
-request('https://graph.facebook.com/v2.6/1043964249024835?access_token='+verifyToken, function (error, response, body) {
+var topost = 'https://graph.facebook.com/v2.6/1043964249024835?access_token=' + verifyToken
+request(topost, function (error, response, body) {
   if (!error && response.statusCode == 200) {
 	  res = JSON.parse(response.body)
 	  console.log(res.first_name)
